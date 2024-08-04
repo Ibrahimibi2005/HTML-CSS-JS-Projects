9485f902cc4ba57f9b48b7418ee7aaed5436b224
Ibrahim malik <126812610+Ibrahimibi2005@users.noreply.github.com>
2024-08-04 23:17:11 +0500

    Add files via upload
    This CODE generates infinite scroll of pictures by using Api HMTL,CSS,JS

---
+++ INFINITE SCROLL/scroll.js
@@ -0,0 +1,67 @@
+const count = 30;
+const key = "lsggeqUy_w9ACxucv1RxbhM7yEZvfNOZ-if7m8XYchE";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${key}&count=${count}`;
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach(photo => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        loader.hidden = false;
+        getPhotos();
+    }
+});
+
+getPhotos();
